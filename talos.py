623d6b6ea285930ee7de03c20aff88a128af36c7
kilon <thekilon@yahoo.co.uk>
2012-06-11 21:48:31 +0300

    clean up the code abit

--- talos.py
+++ talos.py
@@ -2,28 +2,33 @@ import socket
 import time
 
 server = "adams.freenode.net"
-channel = "#Ephestos" 
-nick = "Ephestos" 
-port = 6667 
+channel = "#Ephestos"
+nick = "Ephestos"
+port = 6667
 symbol = "$"
 blank = ""
+running = True
 
-
-def quitIRC():
+def quit():
+    global running
+    running = False
     print("quiting")
-    con.send(bytes("QUIT "+ CHANNEL +"\n","utf-8"))
+    irc.send(bytes("QUIT "+ CHANNEL +"\n","utf-8"))
 
-def fail():
-    con.send(bytes("PRIVMSG "+ CHANNEL +" :Either you do not have the permission to do that, or that is not a valid command.\n","utf-8"))
+def ping(msg):
+    irc.send("PONG :"+ msg +"\r\n")
 
-con = socket.socket( )
-con.connect((server, port))
-con.send(bytes("USER "+ nick +" "+ nick +" "+ nick +" "+nick+"\r\n","utf-8"))
-con.send(bytes("NICK "+ nick +"\r\n","utf-8"))
-con.send(bytes("JOIN "+ channel +"\r\n","utf-8"))
+def error():
+    irc.send(bytes("PRIVMSG "+ CHANNEL +" :No idea what you talking about.\n","utf-8"))
 
-while 1:
-  line = str(con.recv(2048))
+irc = socket.socket( )
+irc.connect((server, port))
+irc.send(bytes("USER "+ nick +" "+ nick +" "+ nick +" "+nick+"\r\n","utf-8"))
+irc.send(bytes("NICK "+ nick +"\r\n","utf-8"))
+irc.send(bytes("JOIN "+ channel +"\r\n","utf-8"))
+
+while running:
+  line = str(irc.recv(2048))
   line = line.strip("\r\n")
   print(line)
   stoperror = line.split(" ")
@@ -32,7 +37,7 @@ while 1:
         pingmsg = pingcmd[1]
         ping(pingmsg)
   elif "PRIVMSG" in line:
-      
+
       if len(line) < 30:
           print( blank )
       elif len(stoperror) < 4:
@@ -42,7 +47,7 @@ while 1:
           info = complete[1]
           msg = line.split(":", 2)[2] ##the thing that was said
           cmd = msg.split(" ")[0]
-          
+
           print("cmd : ",cmd)
           CHANNEL = info.split(" ")[2] ##channel from which it was said
           print("CHANNEL: ",CHANNEL)
@@ -50,10 +55,10 @@ while 1:
           print("user : ",user)
           arg = msg.split(" ")
           print("arg : ",arg)
-        
-       
-          if "$quit\\r\\n'"==cmd:
-              quitIRC()
-            
+
+
+          if "$quit" in cmd:
+              quit()
+
           elif symbol in cmd:
-              fail()
+              error()
